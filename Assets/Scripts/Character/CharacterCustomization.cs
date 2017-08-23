c9b8c18320c049b8005b1d6faa5a51d02e23eb7e
demi180 <noamweiss@gmail.com>
2017-08-22 18:07:58 -0700

    Increased number of spawn points and number of people spawning

--- Assets/Scripts/Character/CharacterCustomization.cs
+++ Assets/Scripts/Character/CharacterCustomization.cs
@@ -2,6 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class AppearancePreset
+{
+	public int fullBody = -1;
+	public int over = -1;
+	public int top = -1;
+	public int bottom = -1;
+	public int shoes = -1;
+	public int underBottom = -1;
+	public int underTop = -1;
+
+	public Color bodyColor = Color.white;
+	public Color hairColor = Color.white;
+	public bool female;
+}
+
 public class CharacterCustomization : MonoBehaviour
 {
 	public GameObject[] fullBody;
@@ -16,53 +32,88 @@ public class CharacterCustomization : MonoBehaviour
 	public Renderer hair;
 
 	public bool female;
+	bool spawned;
 
 	// Use this for initialization
 	void Awake ()
 	{
-		if ( fullBody == null )
-			fullBody = new GameObject[0];
-		if ( underTop == null )
-			underTop = new GameObject[0];
-		if ( overs == null )
-			overs = new GameObject[0];
+		if ( !spawned )
+		{
+			if ( fullBody == null )
+				fullBody = new GameObject[0];
+			if ( underTop == null )
+				underTop = new GameObject[0];
+			if ( overs == null )
+				overs = new GameObject[0];
+			
+			bool useFullBody = false;
+			
+			if ( fullBody != null && fullBody.Length > 0 )
+				useFullBody = Random.value >= 0.75f;
+			
+			// start by turning EVERYTHING off
+			DisableAll ();
+			
+			int index;
+			if ( useFullBody )
+			{
+				index = Random.Range ( 0, fullBody.Length );
+				fullBody [ index ].SetActive ( true );
+				
+			} else
+			{
+				index = Random.Range ( 0, tops.Length );
+				tops [ index ].SetActive ( true );
+				index = Random.Range ( 0, bottoms.Length );
+				bottoms [ index ].SetActive ( true );
+				if ( overs != null && overs.Length > 0 && Random.value > 0.75f )
+				{
+					index = Random.Range ( 0, overs.Length );
+					overs [ index ].SetActive ( true );
+				}
+			}
+			
+			index = Random.Range ( 0, shoes.Length );
+			shoes [ index ].SetActive ( true );
+			
+			hair.material.color = Random.ColorHSV ();
+			body.material.color = Color.white * Random.Range ( 0.15f, 1f );
+		}
+	}
 
-		bool useFullBody = false;
-
-		if ( fullBody != null && fullBody.Length > 0 )
-			useFullBody = Random.value >= 0.75f;
-
-		// start by turning EVERYTHING off
+	void DisableAll ()
+	{
 		fullBody.ForEach ( x => x.SetActive ( false ) );
 		tops.ForEach ( x => x.SetActive ( false ) );
 		bottoms.ForEach ( x => x.SetActive ( false ) );
 		underTop.ForEach ( x => x.SetActive ( false ) );
 		underBottom.ForEach ( x => x.SetActive ( false ) );
 		shoes.ForEach ( x => x.SetActive ( false ) );
+	}
 
-		int index;
-		if ( useFullBody )
+	public void SetAppearance (AppearancePreset preset)
+	{
+		DisableAll ();
+
+		if ( preset.fullBody != -1 )
 		{
-			index = Random.Range ( 0, fullBody.Length );
-			fullBody [ index ].SetActive ( true );
+			fullBody [ preset.fullBody ].SetActive ( true );
 
 		} else
 		{
-			index = Random.Range ( 0, tops.Length );
-			tops [ index ].SetActive ( true );
-			index = Random.Range ( 0, bottoms.Length );
-			bottoms [ index ].SetActive ( true );
-			if ( overs != null && overs.Length > 0 && Random.value > 0.75f )
-			{
-				index = Random.Range ( 0, overs.Length );
-				overs [ index ].SetActive ( true );
-			}
+			if ( preset.top != -1 )
+				tops [ preset.top ].SetActive ( true );
+			if ( preset.bottom != -1 )
+				bottoms [ preset.bottom ].SetActive ( true );
+			if ( preset.over != -1 )
+				overs [ preset.over ].SetActive ( true );
 		}
 
-		index = Random.Range ( 0, shoes.Length );
-		shoes [ index ].SetActive ( true );
+		if ( preset.shoes != -1 )
+			shoes [ preset.shoes ].SetActive ( true );
 
-		hair.material.color = Random.ColorHSV ();
-		body.material.color = Color.white * Random.Range ( 0.15f, 1f );
+		body.material.color = preset.bodyColor;
+		hair.material.color = preset.hairColor;
+		spawned = true;
 	}
 }
--- Assets/Scripts/Character/SpawnPointSpawner.cs
+++ Assets/Scripts/Character/SpawnPointSpawner.cs
@@ -10,13 +10,28 @@ public class SpawnPointSpawner : MonoBehaviour
 	public OrbitCamera followCam;
 	public bool spawnNewPeople;
 	public float spawnTimer = 60;
+	public int spawnCount = 1;
 
+	public bool usePresets;
+	public AppearancePreset[] presets;
+
+	List<GameObject> activePeople;
+	List<float> spawnTimers;
 	float nextSpawnTime;
+	int defaultLayer;
 
 	void Awake ()
 	{
-		SpawnPerson ();
+		activePeople = new List<GameObject> ();
+		spawnTimers = new List<float> ();
+		spawnPoints = GetComponentsInChildren<Transform> ( false );
 		nextSpawnTime = Time.time + spawnTimer;
+		defaultLayer = LayerMask.NameToLayer ( "Default" );
+		SpawnPerson ();
+		for ( int i = 0; i < spawnCount - 1; i++ )
+		{
+			SpawnPerson ( false );
+		}
 	}
 
 	void Update ()
@@ -28,23 +43,65 @@ public class SpawnPointSpawner : MonoBehaviour
 				SpawnPerson ();
 				nextSpawnTime = Time.time + spawnTimer;
 			}
+			for ( int i = spawnTimers.Count - 1; i > 0; i-- )
+			{
+				if ( Time.time > spawnTimers[i] )
+				{
+					SpawnPerson ( false, i );
+				}
+			}
 		}
 	}
 
-	void SpawnPerson ()
+	void SpawnPerson (bool isTarget = true, int activeIndex = -1)
 	{
-		if ( targetInstance != null )
-			Destroy ( targetInstance.gameObject );
 		Transform target = spawnTargets [ Random.Range ( 0, spawnTargets.Length ) ];
 		Transform spawn = GetRandomPoint ();
-		targetInstance = Instantiate ( target );
-		targetInstance.position = spawn.position;
-		targetInstance.gameObject.SetActive ( true );
-		followCam.target = targetInstance;
+		if ( isTarget )
+		{
+			if ( targetInstance != null )
+				Destroy ( targetInstance.gameObject );
+			targetInstance = Instantiate ( target );
+			targetInstance.position = spawn.position;
+			targetInstance.gameObject.SetActive ( true );
+			followCam.target = targetInstance;
+			
+		} else
+		{
+			
+			Transform person = Instantiate ( target );
+			person.position = spawn.position;
+//			person.GetComponent<CharacterCustomization> ().SetAppearance ( presets [ 0 ] );
+			person.gameObject.SetActive ( true );
+			if ( activeIndex != -1 )
+			{
+				Destroy ( activePeople [ activeIndex ] );
+				activePeople [ activeIndex ] = person.gameObject;
+				spawnTimers [ activeIndex ] = Time.time + Random.Range ( 55f, 150f );
+				
+			} else
+			{
+				activePeople.Add ( person.gameObject );
+				spawnTimers.Add ( Time.time + Random.Range ( 55f, 150f ) );
+			}
+			SetLayerRecursively ( person, defaultLayer );
+		}
 	}
 
 	Transform GetRandomPoint ()
 	{
 		return spawnPoints [ Random.Range ( 0, spawnPoints.Length ) ];
+	}
+
+	void SetLayerRecursively (Transform t, int layer)
+	{
+		t.gameObject.layer = layer;
+		if ( t.childCount > 0 )
+		{
+			foreach ( Transform c in t )
+			{
+				SetLayerRecursively ( c, layer );
+			}
+		}
 	}
 }
