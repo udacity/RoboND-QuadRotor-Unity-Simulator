1783e6e3a14bf161daa9c6ceac09baab3eb13999
demi180 <noamweiss@gmail.com>
2017-08-25 15:17:16 -0700

    Fixed telemetry. server will now emit message sensor_frame

--- Assets/Scripts/CommandServer.cs
+++ Assets/Scripts/CommandServer.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Collections;
 using SocketIO;
-using UnityStandardAssets.Vehicles.Car;
+//using UnityStandardAssets.Vehicles.Car;
 using System;
-using System.Security.AccessControl;
+//using System.Security.AccessControl;
 using UnityEngine.UI;
+using System.Threading;
 
 public class CommandServer : MonoBehaviour
 {
@@ -20,9 +21,12 @@ public class CommandServer : MonoBehaviour
 	Texture2D inset2Tex;
 //	Texture2D inset3Tex;
 
+	Thread broadcastThread;
+	int broadcastFrequency;
+
 	DateTime origin = new DateTime (1970, 1, 1, 0, 0, 0);
 
-	void Start()
+	void Start ()
 	{
 		_socket = GetComponent<SocketIOComponent> ();
 		_socket.On ( "open", OnOpen );
@@ -32,10 +36,33 @@ public class CommandServer : MonoBehaviour
 		inset1Tex = new Texture2D ( 1, 1 );
 		inset2Tex = new Texture2D ( 1, 1 );
 //		inset3Tex = new Texture2D ( 1, 1 );
+
+		broadcastFrequency = 60;
+		broadcastThread = new Thread ( ThreadFunc );
+		broadcastThread.Start ();
+		Debug.Log ( "starting" );
+//		EmitTelemetry ();
 	}
 
 	void Update ()
 	{
+//		EmitTelemetry (); // sending in thread instead
+	}
+
+	void OnDestroy ()
+	{
+		broadcastThread.Abort ();
+	}
+
+	void ThreadFunc ()
+	{
+		int sleepTime = (int) ( 1000f / broadcastFrequency );
+
+		while ( true )
+		{
+			EmitTelemetry ();
+			Thread.Sleep ( sleepTime );
+		}
 	}
 
 	void OnOpen(SocketIOEvent obj)
@@ -49,7 +76,7 @@ public class CommandServer : MonoBehaviour
 		Debug.Log ( "Object detected" );
 
 		JSONObject json = obj.data;
-		int[] coords = {
+		int[] camera_coords = {
 			(int) json.GetField ( "x" ).n,
 			(int) json.GetField ( "y" ).n
 		};
@@ -111,110 +138,26 @@ public class CommandServer : MonoBehaviour
 		_socket.Emit ( "Ack", obj );
 	}
 
-/*	void OnSteer(SocketIOEvent obj)
-	{
-//		Debug.Log ( "Steer" );
-		JSONObject jsonObject = obj.data;
-
-		// try to load image1
-		bool loaded = false;
-		Vector2 size = Vector2.zero;
-		Texture2D tex = new Texture2D ( 1, 1 );
-		string imageInfo = "";
-		byte[] imageBytes = null;
-		if ( jsonObject.HasField ( "inset_image" ) )
-			imageInfo = jsonObject.GetField ( "inset_image" ).str;
-		if ( !string.IsNullOrEmpty ( imageInfo ) )
-			imageBytes = Convert.FromBase64String ( imageInfo );
-		if ( imageBytes != null && imageBytes.Length != 0 )
-			loaded = tex.LoadImage ( imageBytes, true );
-		if ( loaded && inset1 != null && tex.width > 1 && tex.height > 1 )
-		{
-			inset1Tex = tex;
-			inset1.texture = inset1Tex;
-			size = inset1.rectTransform.sizeDelta;
-			size.x = 1f * inset1Tex.width / inset1Tex.height * size.y;
-			inset1.rectTransform.sizeDelta = size;
-			inset1.CrossFadeAlpha ( 1, 0.3f, false );
-		} else
-		if ( inset1 != null )
-		{
-//			if ( tex.width == 1 || tex.height == 1 )
-//				inset1.CrossFadeAlpha ( 0, 0.0f, true );
-		}
-		// try to load image2
-		loaded = false;
-		tex = new Texture2D ( 1, 1 );
-		imageInfo = "";
-		imageBytes = null;
-		if ( jsonObject.HasField ( "inset_image2" ) )
-			imageInfo = jsonObject.GetField ( "inset_image2" ).str;
-		if ( !string.IsNullOrEmpty ( imageInfo ) )
-			imageBytes = Convert.FromBase64String ( imageInfo );
-		if ( imageBytes != null && imageBytes.Length != 0 )
-			loaded = tex.LoadImage ( imageBytes, true );
-		if ( loaded && inset2Tex != null && tex.width > 1 && tex.height > 1 )
-		{
-			inset2Tex = tex;
-			inset2.texture = inset2Tex;
-			size = inset2.rectTransform.sizeDelta;
-			size.x = 1f * inset2Tex.width / inset2Tex.height * size.y;
-			inset2.rectTransform.sizeDelta = size;
-			inset2.CrossFadeAlpha ( 1, 0.3f, false );
-		} else
-		if ( inset2 != null )
-		{
-//			if ( tex.width == 1 || tex.height == 1 )
-//				inset2.CrossFadeAlpha ( 0, 0.0f, true );
-		}
-		// try to load image3
-		loaded = false;
-		tex = new Texture2D ( 1, 1 );
-		imageInfo = "";
-		imageBytes = null;
-		if ( jsonObject.HasField ( "inset_image3" ) )
-			imageInfo = jsonObject.GetField ( "inset_image3" ).str;
-		if ( !string.IsNullOrEmpty ( imageInfo ) )
-			imageBytes = Convert.FromBase64String ( imageInfo );
-		if ( imageBytes != null && imageBytes.Length != 0 )
-			loaded = tex.LoadImage ( imageBytes, true );
-		if ( loaded && inset3Tex != null && tex.width > 1 && tex.height > 1 )
-		{
-			inset3Tex = tex;
-			inset3.texture = inset3Tex;
-			size = inset3.rectTransform.sizeDelta;
-			size.x = 1f * inset3Tex.width / inset3Tex.height * size.y;
-			inset3.rectTransform.sizeDelta = size;
-			inset3.CrossFadeAlpha ( 1, 0.3f, false );
-		} else
-		if ( inset3 != null )
-		{
-//			if ( tex.width == 1 || tex.height == 1 )
-//				inset3.CrossFadeAlpha ( 0, 0.0f, true );
-		}
-		EmitTelemetry(obj);
-	}*/
-
 	void EmitTelemetry ()
 	{
 //		Debug.Log ( "Emitting" );
 		UnityMainThreadDispatcher.Instance ().Enqueue ( () =>
 		{
-			print ( "Attempting to Send..." );
+//			print ( "Attempting to Send..." );
 
 			// Collect Data from the Car
 			Dictionary<string, string> data = new Dictionary<string, string> ();
 
 			data [ "timestamp" ] = Timestamp ().ToString ();
 			Vector3 v = quad.Position.ToRos ();
-			data [ "position" ] = v.x.ToString ( "N4" ) + "," + v.y.ToString ( "N4" ) + "," + v.z.ToString ( "N4" );
-			v = ( -quad.Rotation.eulerAngles ).ToRos ();
-			data [ "rpy" ] = v.x.ToString ( "N4" ) + "," + v.y.ToString ( "N4" ) + "," + v.z.ToString ( "N4" );
+			Vector3 v2 = ( -quad.Rotation.eulerAngles ).ToRos ();
+			data [ "pose" ] = v.x.ToString ( "N4" ) + "," + v.y.ToString ( "N4" ) + "," + v.z.ToString ( "N4" ) + "," + v2.x.ToString ( "N4" ) + "," + v2.y.ToString ( "N4" ) + "," + v2.z.ToString ( "N4" );
 			data [ "rgb" ] = Convert.ToBase64String ( CameraHelper.CaptureFrame ( colorCam ) );
 			data [ "depth" ] = Convert.ToBase64String ( CameraHelper.CaptureFrame ( depthCam ) );
 
 //			Debug.Log ("sangle " + data["steering_angle"] + " vert " + data["vert_angle"] + " throt " + data["throttle"] + " speed " + data["speed"] + " image " + data["image"]);
-			_socket.Emit ( "telemetry", new JSONObject ( data ) );
+			_socket.Emit ( "sensor_frame", new JSONObject ( data ) );
+//			Debug.Log ("Emitted");
 		} );
 	}
 
