75ff4b881e4d9d86148f5f307906b359211b5897
demi180 <noamweiss@gmail.com>
2017-09-12 15:47:57 -0700

    Added back recording in training. Fixed shader assignment for masking.

--- Assets/Scripts/GimbalCamera.cs
+++ Assets/Scripts/GimbalCamera.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -16,12 +17,15 @@ public class GimbalCamera : MonoBehaviour
 	[Range (5, 180)]
 	public float sweepCone = 90;
 	public float sweepSpeed = 0.5f;
-//	public bool verticalSweep;
-//	public float vSweepCone = 45;
+
+	public Shader whiteShader;
+	public float timeScale = 1;
+	public float recordFrequency = 0.33f;
 
 
-
-	Camera cam;
+	Camera cam1;
+	Camera cam2;
+	Camera cam3;
 	Transform tr;
 	Quaternion initialLocalRotation;
 
@@ -33,6 +37,13 @@ public class GimbalCamera : MonoBehaviour
 
 	Transform vizSphere;
 
+	float lastTimeChange;
+	string timeLabel;
+	bool recording;
+	float nextRecordTime;
+	int imageCount;
+	bool focusTarget;
+
 	void Awake ()
 	{
 		tr = colorCam.transform;
@@ -41,6 +52,17 @@ public class GimbalCamera : MonoBehaviour
 		initialLocalRotation = gimbalParent.localRotation;
 		lastRotation = gimbalParent.rotation;
 		followType = FollowType.None;
+		cam1 = colorCam.GetComponent<Camera> ();
+		cam2 = maskCam.GetComponent<Camera> ();
+		cam3 = maskCam2.GetComponent<Camera> ();
+		cam2.SetReplacementShader ( whiteShader, "" );
+		cam3.SetReplacementShader ( whiteShader, "" );
+
+		Time.timeScale = timeScale;
+		recordFrequency = 1f / recordFrequency;
+		RecordingController.BeginRecordCallback = OnBeginRecording;
+		RecordingController.EndRecordCallback = OnEndRecording;
+
 //		vizSphere = GameObject.CreatePrimitive ( PrimitiveType.Sphere ).transform;
 //		vizSphere.localScale = Vector3.one * 0.5f;
 //		vizSphere.GetComponent<Renderer> ().material.color = Color.blue * 0.5f;
@@ -115,6 +137,104 @@ public class GimbalCamera : MonoBehaviour
 
 		Position = tr.position;
 		Rotation = tr.rotation;
+
+		// adjust time scale
+		int key0 = (int) KeyCode.Alpha0;
+		for ( int i = 0; i < 10; i++ )
+		{
+			if ( Input.GetKeyDown ( (KeyCode) key0 + i ) )
+			{
+				timeScale = i;
+				lastTimeChange = Time.unscaledTime;
+				timeLabel = "Speed : ^0x".Replace ( "^0", i.ToString () );
+				Time.timeScale = timeScale;
+			}
+		}
+
+		if ( recording )
+		{
+			if ( Time.time >= nextRecordTime )
+			{
+				WriteImage ();
+				nextRecordTime = Time.time + recordFrequency;
+			}
+		}
+	}
+
+	void OnGUI ()
+	{
+		float delta = Time.unscaledTime - lastTimeChange;
+		if ( delta < 2 )
+		{
+			float a = 0;
+			if ( delta < 0.2f )
+				a = delta * 5;
+			else
+				if ( delta > 1.6f )
+					a = ( 2f - delta ) * 2.5f;
+				else
+					a = 1;
+
+			int fontSize = GUI.skin.label.fontSize;
+			GUI.skin.label.fontSize = 40 * Screen.height / 1080;
+			GUI.color = new Color ( 0, 0, 0, a / 2 );
+			Rect r = new Rect ( 5, 5, 500, 100 );
+			GUI.Label ( new Rect ( r.x + 1, r.y + 1, r.width, r.height ), timeLabel );
+			GUI.color = new Color ( 1, 1, 1, a );
+			GUI.Label ( r, timeLabel );
+			GUI.skin.label.fontSize = fontSize;
+		}
+
+//		GUI.Box ( new Rect ( 5, Screen.height - 25, 60, 20 ), "" );
+//		GUI.Label ( new Rect ( 10, Screen.height - 25, 60, 20 ), "Input " + (  ? "on" : "off" ) );
+	}
+
+	void OnBeginRecording ()
+	{
+		recording = true;
+	}
+
+	void OnEndRecording ()
+	{
+		recording = false;
+	}
+
+	void WriteImage ()
+	{
+		string prefix = imageCount.ToString ( "D5" );
+//		Debug.Log ( "writing " + prefix );
+		imageCount++;
+		// needed to force camera update 
+		RenderTexture targetTexture = cam1.targetTexture;
+		RenderTexture.active = targetTexture;
+		byte[] bytes;
+		Texture2D tex = new Texture2D ( targetTexture.width, targetTexture.height, TextureFormat.RGB24, false );
+		tex.ReadPixels ( new Rect ( 0, 0, targetTexture.width, targetTexture.height ), 0, 0 );
+		tex.Apply ();
+		bytes = tex.EncodeToPNG ();
+		string directory = RecordingController.SaveLocation;
+		string path = Path.Combine ( directory, "cam1_" + prefix + ".png" );
+		File.WriteAllBytes ( path, bytes );
+
+		targetTexture = cam2.targetTexture;
+		RenderTexture.active = targetTexture;
+		tex.ReadPixels ( new Rect ( 0, 0, targetTexture.width, targetTexture.height ), 0, 0 );
+		tex.Apply ();
+		bytes = tex.EncodeToPNG ();
+		path = Path.Combine ( directory, "cam2_" + prefix + ".png" );
+		File.WriteAllBytes ( path, bytes );
+
+		targetTexture = cam3.targetTexture;
+		RenderTexture.active = targetTexture;
+		tex.ReadPixels ( new Rect ( 0, 0, targetTexture.width, targetTexture.height ), 0, 0 );
+		tex.Apply ();
+		bytes = tex.EncodeToPNG ();
+		path = Path.Combine ( directory, "cam3_" + prefix + ".png" );
+		File.WriteAllBytes ( path, bytes );
+
+		bytes = null;
+		RenderTexture.active = null;
+		Destroy ( tex );
 	}
 
 	public void LookAt (Vector3 position)
